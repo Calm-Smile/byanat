ed3acff77d8aa836f02a4bc846802747b5dd8ebf
malghawi <malik6120@gmail.com>
2023-05-07 01:08:16 +0400

    Add existing file

--- Backend (spring boot)/src/main/java/com/example/ByanPrjt/ArrayT.java
+++ Backend (spring boot)/src/main/java/com/example/ByanPrjt/ArrayT.java
@@ -2,10 +2,8 @@ package com.example.ByanPrjt;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.lang.reflect.Array;
+//@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape=JsonFormat.Shape.ARRAY)
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record ArrayT (Tower ar)
 {}
 
